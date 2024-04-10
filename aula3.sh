d154d4dc599b1857c2a9c450db91e2bfd23a6660
Júlia Martins Souza <juliamss07@gmail.com>
2024-04-10 14:32:05 -0300

    Update aula3.sh
    finish some tests in my virtual box

--- aula3.sh
+++ aula3.sh
@@ -3,15 +3,16 @@
 
 # 11. Escreva um script que realize as seguintes tarefas:
 # a) Mostre a versao do Ubuntu instalada;
-
+lsb_release -a
 
 # b) Verifique o status do servidor SSH;
-
+sudo service ssh status
 
 # c) Um traceroute para www.ufmg.br;
-
+traceroute www.ufmg.br
 
 # d) Atualizacao (update) do ubuntu;
-
+apt upgrade
 
 # e) Desligar a maquina virtual;
+sudo poweroff
